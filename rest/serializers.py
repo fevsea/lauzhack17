b137611bb7a80efad5cada54ee16ed1021ccb02c
Alejandro Agustin B <alejandro.a.es@ieee.org>
2017-11-12 07:18:24 +0100

    serializers cleanup

--- rest/serializers.py
+++ rest/serializers.py
@@ -13,11 +13,9 @@ class ItemSerializer(serializers.ModelSerializer):
 
 class MissionSerializer(serializers.ModelSerializer):
     highlight = serializers.HyperlinkedIdentityField(view_name='mission_detail')
-    web = ItemSerializer(many=True, read_only=True)
     class Meta:
         model = Mission
-        #fields = ('pk', 'name', 'description', 'url', 'img', 'highlight')
-        fields = ('pk', 'name', 'description', 'url', 'img', 'highlight', 'web')
+        fields = ('pk', 'name', 'description', 'url', 'img', 'highlight')
 
 class MissionSerializerDetail(serializers.ModelSerializer):
     web = ItemSerializer(many=True, read_only=True)
